a0dcbab0587a79c79415d4b45fb6a91e4bfc9458
jejikeh <jejikeh@gmail.com>
2022-08-24 14:06:46 +0300

    add moniori dll reference

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ static class Program
 {
     static bool IsCommon(Window window)
     {
-
         // https://github.com/glsorre/amethystwindows/blob/master/AmethystWindows/DesktopWindowsManager/DesktopWindow.cs#L46
         return window.WindowInfo.dwExStyle.HasFlag(NativeWin32UserApiStructs.WindowStylesEx.WS_EX_WINDOWEDGE) &&
                window.WindowInfo.dwStyle.HasFlag(NativeWin32UserApiStructs.WindowStyles.WS_MINIMIZEBOX) &&
@@ -23,13 +22,14 @@ static class Program
             if (IsCommon(win))
             {
                 canTiledWindows.Add(win);
+                Console.WriteLine(WrappedWin32UserApi.GetMonitorInfo(win.MonitorHandle).WorkArea.ToString());
             }
         });
 
-        canTiledWindows.ForEach(t =>
+        canTiledWindows.ForEach(win =>
         {
-            WrappedWin32UserApi.ShowWindow(t.Handle, NativeWin32UserApiStructs.ShowWindowMode.SW_NORMAL);
-            WrappedWin32UserApi.MoveWindow(t.Handle, 0, 0, 200, 200, true);
+            //WrappedWin32UserApi.ShowWindow(win.Handle, NativeWin32UserApiStructs.ShowWindowMode.SW_NORMAL);
+            //WrappedWin32UserApi.MoveWindow(win.Handle, 0, 0, 200, 200, true);
         });
     }
 }
